c530ef408127b4007f6fb6d4a05f847f267bf241
AbdulRehman1632 <a.rehman1632@gmail.com>
2025-08-14 17:08:24 +0500

    D working on offer

--- src/utils/constant/CustomCrudBanner/CustomCrudBanner.jsx
+++ src/utils/constant/CustomCrudBanner/CustomCrudBanner.jsx
@@ -6,6 +6,11 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Loader from '../Loader/Loader';
 
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Loader from '../Loader/Loader';
+
 const CustomCrudBanner = () => {
   const [offers, setOffers] = useState([]);
     const [loading, setLoading] = useState(true);
