3fefca9a48cebf01fdfd65c85fc010a7a56dd0a6
AbdulRehman1632 <a.rehman1632@gmail.com>
2025-08-15 16:13:27 +0500

    adding backend url

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const apiClient = axios.create({
-    baseURL:'http://localhost:2004',
-    timeout : 6000,
+    baseURL: ["https://perfumes-backend.vercel.app/", "http://localhost:2004"],
+    timeout : 15000,
     headers:{
         "Content-Type":"application/json"
     }
